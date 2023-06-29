711eee1ff5ed7b9a77b18ffee2ae3828b1380fd8
Priya Chaudhary <priya22diswar@gmail.com>
2023-06-29 20:55:22 +0530

    Added tictactoe's skeleton

---
+++ tictactoe.py
@@ -0,0 +1,30 @@
+def print_screen():
+    '''prints whole screen depending upon condition'''
+    pass
+
+def print_game_screen():
+    pass
+
+def check_game_result():
+    pass
+
+def check_game_isdraw():
+    pass
+
+def check_game_iswinner(sign):
+    pass
+
+def take_input():
+    pass
+
+def validate_input(inp):
+    pass
+
+def make_move():
+    pass
+
+def play_game():
+    while True:
+        print_game_screen()
+        make_move()
+play_game()
