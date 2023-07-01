98877ffc2f6cfe67cfaf6f52f562320824109d85
Priya Chaudhary <priya22diswar@gmail.com>
2023-07-01 13:18:20 +0530

    Adding few conditions of winning

--- tictactoe.py
+++ tictactoe.py
@@ -12,6 +12,7 @@ def check_game_isdraw():
     pass
 
 def check_game_iswinner(sign):
+    # todo: this function is not complete yet, some conditions are not included now
     global board
     for i in range(3):
         if board[i][0]==board[i][1]==board[i][2]:
