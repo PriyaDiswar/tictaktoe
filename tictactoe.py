267bca58beb55e476e9752c1c5949665871f396f
Priya Chaudhary <priya22diswar@gmail.com>
2023-07-02 15:50:30 +0530

    Added the wnining and draw conditoins

--- tictactoe.py
+++ tictactoe.py
@@ -32,15 +32,40 @@ def check_game_result():
     pass
 
 def check_game_isdraw():
-    pass
+    res=True
+    for i in range(3):
+        for j in range(3):
+            a=BOARD[i][j].isalpha()
+            res=res and a
+
+    p=check_game_iswinner("x")
+    q=check_game_iswinner("O")
+    if res==True and p==False and q==False:
+        return True
+    else:
+        return False
+
 
 def check_game_iswinner(sign):
     # todo: this function is not complete yet, some conditions are not included now
-    global board
+    win=False
+    #for row
     for i in range(3):
-        if board[i][0]==board[i][1]==board[i][2]:
-            return True
-        
+        if BOARD[i][0]==BOARD[i][1]==BOARD[i][2]==sign:
+            win=True
+            break
+    #for column
+        elif BOARD[0][i]==BOARD[1][i]==BOARD[2][i]==sign:
+            win=True
+            break
+    #for diagonal
+    if BOARD[0][0]==BOARD[1][1]==BOARD[2][2]==sign:
+        win=True
+    elif BOARD[0][2]==BOARD[1][1]==BOARD[2][0]==sign:
+        win=True
+
+    return win
+
 
 def take_input():
     a=int(input("Please enter your choice : "))
